7bbfbee931410e96b777f8e58be4bd5ff6e3345d
Omar Aidi <76522937+Aiomar@users.noreply.github.com>
2025-01-02 14:36:39 +0100

    Update Footer.jsx

--- frontend/src/components/home/Footer.jsx
+++ frontend/src/components/home/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="md:h-16 bg-slate-100 dark:bg-gray-900">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-xl dark:text-white font-mono text-center ">
-          © 2024{" "}
+          © 2025{" "}
           <a href="" className="hover:underline">
             {" "}
             Made by Omar Aidi
