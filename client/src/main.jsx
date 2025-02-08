14529af4e6852033634c27ddaba645c06d9c6eb9
Omar Aidi <aidi360omar@gmail.com>
2025-02-08 16:56:34 +0100

    added vercel  <SpeedInsights /> tag and packge

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,13 @@ import React from "react";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    <SpeedInsights />
   </React.StrictMode>
 );
