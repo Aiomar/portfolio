2f9755f5db3dc97513f10317cd070190ab800240
Omar Aidi <aidi360omar@gmail.com>
2025-02-11 13:34:37 +0100

    edit light mode message text in contact

--- client/src/components/home/Suggestion.jsx
+++ client/src/components/home/Suggestion.jsx
@@ -49,7 +49,7 @@ const Form = forwardRef(({ id, visible }, ref) => {
               name="fullName"
               className="w-full px-5 py-2 mt-2 border border-gray-500 rounded-md focus:outline-none  focus:border-none 
                 focus:ring-2 focus:ring-sky-500 roboto-regular dark:bg-slate-800 
-              dark:placeholder:text-gray-200 text-white"
+              dark:placeholder:text-gray-200 text-black"
               placeholder="Enter your Name"
               required
             />
@@ -68,7 +68,7 @@ const Form = forwardRef(({ id, visible }, ref) => {
               name="phone"
               className="w-full px-5 py-2 mt-2 border border-gray-500 rounded-md focus:outline-none  
                 focus:border-none focus:ring-2 focus:ring-sky-500 roboto-regular dark:bg-slate-800
-                dark:placeholder:text-gray-200 text-white"
+                dark:placeholder:text-gray-200 text-black"
               placeholder="+216"
               required
             />
@@ -87,7 +87,7 @@ const Form = forwardRef(({ id, visible }, ref) => {
               name="email"
               className="w-full px-5 py-2 mt-2 border border-gray-500 rounded-md focus:outline-none  
                 focus:border-none  focus:ring-2 focus:ring-sky-500 dark:bg-slate-800 dark:placeholder:text-gray-200
-                text-white"
+                text-black"
               placeholder="Enter your email"
               required
             />
