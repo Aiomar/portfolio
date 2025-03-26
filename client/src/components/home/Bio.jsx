4c3082958852e7df86674e26355be4d2b418eeda
Omar Aidi <76522937+Aiomar@users.noreply.github.com>
2025-03-26 16:50:19 +0100

    Update Bio.jsx

--- client/src/components/home/Bio.jsx
+++ client/src/components/home/Bio.jsx
@@ -34,14 +34,14 @@ const Bio = forwardRef(({ id, visible }, ref) => {
         <div className="relative">
           <img
             src="/uploads/profile.png"
-            className="hidden rounded-full w-36 sm:w-44 md:w-52 border-4 border-black dark:border-gray-200 shadow-lg shadow-gray-400
-              dark:shadow-gray-800 md:float-right md:ml-6 md:flex "
+            className="hidden rounded-full w-36 sm:w-44 md:w-52 shadow-lg shadow-gray-400
+            dark:shadow-gray-800 md:float-right md:ml-6 md:flex "
             alt="Omar Aidi"
           />
           <div className="md:hidden flex flex-col items-center mb-3">
             <img
               src="/uploads/profile.png"
-              className="rounded-full w-36 sm:w-44 md:w-52 border-4 border-black dark:border-gray-200 shadow-lg shadow-gray-400
+              className="rounded-full w-36 sm:w-44 md:w-52 border-4  shadow-lg shadow-gray-400
               dark:shadow-gray-800 md:float-right md:ml-6 "
               alt="Omar Aidi"
             />
